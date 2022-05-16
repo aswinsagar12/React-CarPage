af5184e61895246d7d0e7e43e343fb854509e4e1
aswinsagar12 <aswinsagar12@gmail.com>
2022-05-16 17:05:13 +0530

    locomotiveScroll Added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, { useRef, useEffect } from 'react'
 import './App.scss';
 import Footer from './components/Footer/Footer'
 import Gallery from './components/Gallery/Gallery';
+import LocomotiveScroll from 'locomotive-scroll';
+import '../node_modules/locomotive-scroll/src/locomotive-scroll.scss'
 import { imageData } from './Data'
 
 const App = () => {
+  const ref = useRef(null)
+
+  useEffect(() => {
+    if (ref) {
+      new LocomotiveScroll({
+        el: ref.current,
+        smooth: true,
+        direction: 'horizontal',
+        multiplier: .5
+      })
+    }
+
+
+  }, [])
+
 
   const images = imageData.map((tuples, index) => (
     tuples.map((url, elementIndex) =>
@@ -19,15 +36,15 @@ const App = () => {
   return (
     <>
       <div className="main-container">
-        <div className='scroll-container'>
+        <div className='scroll-container' data-scroll-container ref={ref}>
           <div className='content'>
             <div className='gallery'>
               {images}
               <div className='gallery-helper'>Scroll Discover</div>
-              <div className='behind-text fill'>
+              <div className='behind-text fill' data-scroll data-scroll-speed={-1}>
                 Brew Your Coffee,
               </div>
-              <div className='behind-text outline'>
+              <div className='behind-text outline' data-scroll data-scroll-speed={-1}>
                 Drink Your Coffee
               </div>
             </div>
--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -5,10 +5,19 @@ import { points } from '../../Data'
 function Gallery({ src, index, columnOffset }) {
     const values = points[index];
     if (!values) return null;
-    const [row, column, spanRow, spanColumn] = values
+    const [row, column, spanRow, spanColumn] = values;
+
+    const getScrollindex = () => {
+        if (index === 1 || index === 4) return -1
+        if (index === 0 || index === 3) return 0
+        return 1;
+    }
 
     return (
         <div className="gallery-item"
+            data-Scroll
+            data-scroll-speed={getScrollindex()}
+
             style={{
                 gridArea: `${row} / ${column + columnOffset}
                 /span ${spanRow} / span ${spanColumn}`,
